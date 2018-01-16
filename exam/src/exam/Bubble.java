4a59d31bbc0fbcefa977531ca35190c2815f2c52
kmg <kmg31815@gmail.com>
2018-01-16 14:28:46 +0800

    Niner,Bubble

---
+++ exam/src/exam/Bubble.java
@@ -0,0 +1,21 @@
+package exam;
+
+public class Bubble {
+
+	public static void main(String[] args) {
+		int n[] = { 57, 21, 36, 14, 42 };
+		for (int i = 0; i < n.length - 1; i++) {
+			for (int j = i + 1; j < n.length; j++) {
+				if (n[j] < n[i]) {
+					int tmp = n[i];
+					n[i] = n[j];
+					n[j] = tmp;
+				}
+			}
+		}
+		for (int a : n) {
+			System.out.print(a + "\t");
+		}
+	}
+
+}
